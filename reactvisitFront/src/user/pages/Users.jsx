d0b1785b703653f7d506324545912a0552858599
WinnerPindi <Winner.Mazonzika.Pindi@uqtr.ca>
2024-01-22 22:02:04 -0500

    Ajout des composants et stylisation Css

--- reactvisitFront/src/user/pages/Users.jsx
+++ reactvisitFront/src/user/pages/Users.jsx
@@ -1,5 +1,13 @@
-export default function Users(){
-    return(
-        <h1>Users Works </h1>
-    );
+import UsersList from "../components/UsersList";
+
+export default function Users() {
+  const USERS = [
+    {
+      id: "u1",
+      name: "Elon Musk",
+      image:"https://upload.wikimedia.org/wikipedia/commons/9/99/Elon_Musk_Colorado_2022_%28cropped2%29.jpg",
+      place: 3
+    },
+  ];
+  return <UsersList items={USERS} />;
 }
